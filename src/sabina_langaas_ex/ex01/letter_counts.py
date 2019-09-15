67a252e7a2306232defff39dcc6c8bd2f06c9477
Sabina <sabinal@nmbu.no>
2019-09-15 18:01:00 +0200

    Stage C : Making a code that count frequency

---
+++ src/sabina_langaas_ex/ex01/letter_counts.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+Solution to task C on exercise 01 in INF200
+"""
+
+def letter_freq(txt):
+    """
+    This code counts the frequency of the letters in the string that´s entered by the user of the code
+    :param txt: input for the user of the code, a string
+    :return: returns a dictionary where key is the characters in txt and the value is the number of occurrences in txt
+    """
+    freq = {}
+    txt_lower = txt.lower()
+
+    for char in txt_lower:
+        if char in freq.keys():
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+
+
+if __name__ == '__main__':
+    text = input('Please enter text to analyse: ')
+
+    frequencies = letter_freq(text)
+    frequencies_sorted = dict(sorted(frequencies.items()))
+    for letter, count in frequencies_sorted.items():
+        print('{:3}{:10}'.format(letter, count))
