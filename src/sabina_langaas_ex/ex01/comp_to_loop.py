3c04452ffc7089d0a9b8f0e5b9f4dea660385920
Sabina <sabinal@nmbu.no>
2019-09-15 17:59:34 +0200

    task A : Making a comp out of a loop

---
+++ src/sabina_langaas_ex/ex01/comp_to_loop.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Solution to task B on exercise 01 in INF200
+"""
+
+def squares_by_comp(n):
+    return [k**2 for k in range(n) if k % 3 == 1]
+
+
+def squares_by_loop(n):
+    squares = []
+    for k in range(n):
+        if k % 3 == 1:
+            squares.append(k**2)
+    return squares
+
+
+if __name__ == '__main__':
+
+    how_many_squares: int = 10
+
+    if squares_by_comp(how_many_squares) != squares_by_loop(how_many_squares):
+        print('ERROR!')
