d6c9168b649a5e2a1a50b88dd4c0c83684e2ce6a
Sabina <sabinal@nmbu.no>
2019-09-16 10:47:15 +0200

    Task C: cleaning up my code

--- src/sabina_langaas_ex/ex01/letter_counts.py
+++ src/sabina_langaas_ex/ex01/letter_counts.py
@@ -4,10 +4,13 @@
 Solution to task C on exercise 01 in INF200
 """
 
+
 def letter_freq(txt):
     """
     This code counts the frequency of the letters in the string that´s entered by the user of the code
+
     :param txt: input for the user of the code, a string
+
     :return: returns a dictionary where key is the characters in txt and the value is the number of occurrences in txt
     """
     freq = {}
