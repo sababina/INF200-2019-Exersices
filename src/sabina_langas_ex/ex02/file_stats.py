26723df84619066bca1bf5804194c716aa8167db
Sabina <sabinal@nmbu.no>
2019-09-19 18:24:17 +0200

    Started on task A on ex02

---
+++ src/sabina_langas_ex/ex02/file_stats.py
@@ -0,0 +1,30 @@
+def char_counts(textfilename):
+    open_file = open(textfilename, encoding='utf-8')
+
+    content_of_file = open_file.read()
+
+    converted_file = [ord(letters) for letters in content_of_file]
+    result = [0]*256
+
+    for number in range(len(converted_file)):
+        for char_number in range(256):
+            if converted_file[number] == char_number:
+                result[char_number] += 1
+    return result
+
+
+if __name__ == '__main__':
+
+    filename = 'file_stats.py'
+    frequencies = char_counts(filename)
+    for code in range(256):
+        if frequencies[code] > 0:
+            character = ''
+            if code >= 32:
+                character = chr(code)
+
+            print(
+                '{:3}{:>4}{:6}'.format(
+                    code, character, frequencies[code]
+                )
+            )
