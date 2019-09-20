192bb448acf8955865d1f129b42a45a1120c2233
Sabina <sabinal@nmbu.no>
2019-09-20 08:49:49 +0200

    Started ex02

---
+++ src/sabina_langas_ex/ex02/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(data):
+    pass
+
+if __name__ == "__main__":
+
+    for data in ((),
+                 (1,),
+                 (1, 3, 8, 12),
+                 (12, 8, 3, 1),
+                 (8, 3, 12, 1)):
+        print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
--- src/sabina_langas_ex/ex02/file_stats.py
+++ src/sabina_langas_ex/ex02/file_stats.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
 def char_counts(textfilename):
     open_file = open(textfilename, encoding='utf-8')
-
     content_of_file = open_file.read()
+    result = [0]*256
 
     converted_file = [ord(letters) for letters in content_of_file]
-    result = [0]*256
 
     for number in range(len(converted_file)):
         for char_number in range(256):
