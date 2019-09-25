4d2d273db3de9ab3a0f636c109fa427dda963ba2
Sabina <sabinal@nmbu.no>
2019-09-25 14:05:37 +0200

    Fix everthing

--- src/sabina_langas_ex/ex01/comp_to_loop.py
+++ src/sabina_langas_ex/ex01/comp_to_loop.py
@@ -11,7 +11,7 @@ def squares_by_comp(n):
 def squares_by_loop(n):
     """
     Returns a list containing the squares of all factors of 3 up to n
-    
+
     Arguments:
         n {int} -- Integer which the for loop will count up to
     
