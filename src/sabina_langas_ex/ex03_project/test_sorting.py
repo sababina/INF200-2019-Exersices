8a2620092506ea9935d75ebb57b383346f0e1916
Sabina <sabinal@nmbu.no>
2019-10-02 18:37:33 +0200

    Task A: Solved the last test of task A

--- src/sabina_langas_ex/ex03_project/test_sorting.py
+++ src/sabina_langas_ex/ex03_project/test_sorting.py
@@ -95,4 +95,16 @@ def test_sorting():
     ensure that they are sorted correctly. These could be lists of
     numbers of different length or lists of strings.
     """
-    pass
+    sorted_numbers_int = [3, 4, 5, 6, 7]
+    assert bubble_sort(sorted_numbers_int) == sorted(sorted_numbers_int)
+
+    sorted_string = 'abehehhe'
+    assert bubble_sort(sorted_string) == sorted(sorted_string)
+
+    sorted_string_list = ['aaa', 'bnbb', 'jfjfjff']
+    assert bubble_sort(sorted_string_list) == sorted(sorted_string_list)
+
+    sorted_float = [0.0000, 0.89898, 0.9999]
+    assert bubble_sort(sorted_float) == sorted(sorted_float)
+
+
