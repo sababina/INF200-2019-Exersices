c2bdcbf9261aaf07c87a2652c1cca86a410f43ac
Sabina <sabinal@nmbu.no>
2019-10-02 13:21:30 +0200

    Task B: Chaged the median function, so it passes all the tests

--- src/sabina_langas_ex/ex03_project/test_median.py
+++ src/sabina_langas_ex/ex03_project/test_median.py
@@ -14,11 +14,17 @@ def median(data):
     :param data: An iterable of containing numbers
     :return: Median of data
     """
+    sorted_data = sorted(data)
+    num_elements = len(sorted_data)
 
-    sdata = sorted(data)
-    n = len(sdata)
-    return (sdata[n // 2] if n % 2 == 1
-            else 0.5 * (sdata[n // 2 - 1] + sdata[n // 2]))
+    if num_elements % 2 == 1:
+        return sorted_data[num_elements // 2]
+    elif not sorted_data:
+        raise ValueError
+    else:
+        return 0.5 * (
+                sorted_data[num_elements // 2 - 1] + sorted_data[num_elements // 2]
+        )
 
 
 def test_median_of_singleton():
@@ -26,7 +32,7 @@ def test_median_of_singleton():
 
 
 def test_median_of_odd_number():
-    data =[1, 3, 5, 7, 9]
+    data = [1, 3, 5, 7, 9]
     assert median(data) == statistics.median(data)
 
 
