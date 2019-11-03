9e01aa27cb769e2b5488f9dcaac9a0845eeea288
Sabina <sabinal@nmbu.no>
2019-11-03 19:44:42 +0100

    Task A: Adde the walker class for ex04

--- src/sabina_langas_ex/ex05/walker_sim.py
+++ src/sabina_langas_ex/ex05/walker_sim.py
@@ -2,3 +2,46 @@
 
 __author__ = 'Sabina Langås'
 __email__ = 'sabinal@nmbu.no'
+
+import random
+
+
+class Walker:
+    """
+    Simulates a random walker.
+    A walker starts at a given position and has a home.
+    """
+
+    def __init__(self, start_position, home_position):
+        """
+        :param start_position: The value at where the walker starts
+        :param home_position: The value of where the walker's home is
+        """
+        self.x = start_position
+        self.h = home_position
+        self.steps = 0
+
+    def move(self):
+        """
+        Moves the walker  by +1 or -1 step, with equal probability.
+        """
+        self.x += 2 * random.randint(0, 1) - 1
+        self.steps += 1
+
+    def is_at_home(self):
+        """
+        Returns True, if the walker have reach home safely
+        """
+        return self.x == self.h
+
+    def get_position(self):
+        """
+        Returns the walker's current position
+        """
+        return self.x
+
+    def get_steps(self):
+        """
+        Returns how many steps the walker has taken
+        """
+        return self.steps
