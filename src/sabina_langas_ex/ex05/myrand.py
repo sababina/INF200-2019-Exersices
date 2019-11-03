98d5bc1554def0b499eabe8da5130ad02aec4d64
Sabina <sabinal@nmbu.no>
2019-11-03 20:00:44 +0100

    Task B: Made the class BoundedWalker

--- src/sabina_langas_ex/ex05/myrand.py
+++ src/sabina_langas_ex/ex05/myrand.py
@@ -4,3 +4,35 @@ from walker_sim import Walker, Simulation
 
 __author__ = 'Sabina Langås'
 __email__ = 'sabinal@nmbu.no'
+
+
+class BoundedWalker(Walker):
+
+    def __init__(self, start, home, left_limit, right_limit):
+        """
+        Initialise the walker
+
+        Arguments
+        ---------
+        start : int
+            The walker's initial position
+        home : int
+            The walk ends when the walker reaches home
+        left_limit : int
+            The left boundary of walker movement
+        right_limit : int
+            The right boundary  of walker movement
+        """
+
+        super().__init__(start, home)
+        self.left_limit = left_limit
+        self.right_limit = right_limit
+        self.x = self.x
+
+    def move(self):
+
+        super().move()
+        if self.x < self.left_limit:
+            self.x = self.left_limit
+        elif self.x > self.right_limit:
+            self.x = self.right_limit
