989fdabc2674c0f80c3be592b6ff773e71053e11
Sabina <sabinal@nmbu.no>
2019-11-03 20:15:18 +0100

    Task C: Implemeted the code fro ex04

--- src/sabina_langas_ex/ex05/myrand.py
+++ src/sabina_langas_ex/ex05/myrand.py
@@ -2,3 +2,32 @@
 
 __author__ = 'Sabina Langås'
 __email__ = 'sabinal@nmbu.no'
+
+
+class LCGRand:
+    a = 7 ** 5
+    m = 2 ** 31 - 1
+
+    def __init__(self, seed):
+        self.previous = seed
+
+    def rand(self):
+        self.previous = LCGRand.a * self.previous % LCGRand.m
+
+        return self.previous
+
+    def random_sequence(self, length):
+
+        return RandIter(self, length)
+
+    def infinite_random_sequence(self):
+        """
+        Generate an infinite sequence of random numbers.
+
+        Yields
+        ------
+        int
+            A random number.
+        """
+        while True:
+            yield self.rand()
